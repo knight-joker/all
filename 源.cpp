d7d5875bd1f5c2cfea0b229c0f0b6c326d252347
knight <111353207+knight-joker@users.noreply.github.com>
2022-10-09 08:30:20 +0800

    星期计算机

--- 源.cpp
+++ 源.cpp
@@ -1,14 +1,37 @@
-#include<stdio.h>
-#include<windows.h>
-int main() {
-	for (float y = 1.5f; y > -1.5f; y -= 0.1f) {
-		for (float x = -1.5f; x < 1.5f; x += 0.05f) {
-			float a = x * x + y * y - 1;
-			putchar(a * a * a - x * x * y * y * y < 0.0f ? '*' : ' ');
-		}
-		Sleep(100);
-		system("color 0c");
-		putchar('\n');
+#include <stdio.h>
+int main()
+{
+	int mouth, day,week;
+	printf("请输入月份，再输入日期（中间用空隔隔开）\n");
+	scanf_s("%d %d", &mouth, &day);
+	if (mouth == 2 || mouth == 3 || mouth == 11)
+	{
+		week = (day + 1) % 7;
 	}
+	if(mouth == 1 || mouth == 10);
+	{
+		week = (day + 5) % 7;
+	}
+	if (mouth == 4 || mouth == 7)
+	{
+		week = (day + 4) % 7;
+	}
+	if (mouth == 5)
+	{
+		week = (day + 6) % 7;
+	}
+	if (mouth == 8)
+	{
+		week = day % 7;
+	}
+	if (mouth == 6)
+	{
+		week = (day + 2) % 7;
+	}
+	if (mouth == 9 || mouth == 12)
+	{
+		week = (day + 3) % 7;
+	}
+	printf("今天是2022年%d月%d日，星期%d。\n",mouth,day,week);
 	return 0;
 }
