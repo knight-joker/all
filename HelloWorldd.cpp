4e8a57e6cce24f07dbaed7fe0a98671c7725f46a
knight <111353207+knight-joker@users.noreply.github.com>
2022-10-13 16:06:57 +0800

    HelloWorld

---
+++ HelloWorldd.cpp
@@ -0,0 +1,51 @@
+//#include <stdio.h>
+//int main() //主函数-程序的入口  main函数有且仅有一个    //int是整型的意思
+//{
+	//printf("HelloWorld\n");//函数-printf function-printf-打印函数
+	//
+   // return 0;//返回
+//}
+//main前面的int表示main函数调用返回一个整型值
+   
+
+
+#include<stdio.h>
+int main()
+{
+	//int age = 20;
+	//printf("%d\n", age);
+
+
+
+
+	//float f = 5.0;
+	//printf("%f\n", f);
+
+	//double d = 3.14;
+	//printf("%lf\n", d);
+
+	//printf("%d\n", sizeof(char));
+	//printf("%d\n", sizeof(short));
+	//printf("%d\n", sizeof(int));
+	//printf("%d\n", sizeof(long));
+	//printf("%d\n", sizeof(long long));
+	//printf("%d\n", sizeof(float));
+	//printf("%d\n", sizeof(double))
+	
+
+	
+	//int num1 = 20;
+	//int num2 = 10;
+	//int sum = 0;
+	//scanf_s("%d%d", &num1, &num2);
+	
+    //sum = num1 + num2;
+	//printf("sum=%d\n", sum);
+	
+
+
+
+
+
+	return 0;
+}
